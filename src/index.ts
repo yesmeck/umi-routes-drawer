9f555b6e3602c97ba17c1d6d980b78c09e5824d7
Wei Zhu <yesmeck@gmail.com>
2019-05-09 01:11:39 +0800

    Add resources

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,54 @@
 import { IRoute } from 'umi-types';
 
-const routes: IRoute[] = [];
-const currentRoutes = routes;
+type Block = () => void;
 
-export function draw(block: () => void) {
+interface PageOptions {
+  component: string;
+  redirect?: string;
+}
+
+let routes: IRoute[] = [];
+let currentRoutes = routes;
+
+function reset() {
+  routes = [];
+  currentRoutes = routes;
+}
+
+export function draw(block: Block) {
+  reset();
   block();
   return routes;
 }
 
-export function page(path: string, options: { component: string; redirect?: string }) {
+export function page(path: string, options: PageOptions) {
   currentRoutes.push({
     path,
     ...options
   });
+}
+
+export function resources(resources: string, options?: {}, block?: Block) {
+  currentRoutes.push(
+    {
+      path: `/${resources}`,
+      component: `/pages/${resources}/index`
+    },
+    {
+      path: `/${resources}/new`,
+      component: `/pages/${resources}/new`
+    },
+    {
+      path: `/${resources}/:id`,
+      component: `/pages/${resources}/show`
+    },
+    {
+      page: `/${resources}/:id/edit`,
+      component: `/pages/${resources}/edit`
+    }
+  );
+
+  if (block) {
+    block();
+  }
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,10 +1,44 @@
-import { draw, page } from '../src';
+import { draw, page, resources } from '../src';
 
 describe('drawer', () => {
   it('path', () => {
     const routes = draw(() => {
       page('/', { component: '/pages/index' });
     });
-    expect(routes).toEqual([{ path: '/', component: '/pages/index' }]);
+    expect(routes).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "component": "/pages/index",
+          "path": "/",
+        },
+      ]
+    `);
+  });
+
+  describe('resouces', () => {
+    const routes = draw(() => {
+      resources('posts');
+    });
+
+    expect(routes).toMatchInlineSnapshot(`
+            Array [
+              Object {
+                "component": "/pages/posts/index",
+                "path": "/posts",
+              },
+              Object {
+                "component": "/pages/posts/new",
+                "path": "/posts/new",
+              },
+              Object {
+                "component": "/pages/posts/show",
+                "path": "/posts/:id",
+              },
+              Object {
+                "component": "/pages/posts/edit",
+                "page": "/posts/:id/edit",
+              },
+            ]
+        `);
   });
 });
